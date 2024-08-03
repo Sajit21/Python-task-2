1dbfdabb4870294ddd1b9822ef6639e38f6ffae5
Sajit Maharjan <126838978+Sajit21@users.noreply.github.com>
2024-08-03 19:10:19 +0545

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,20 @@
+
+
+#here we have to display fizz if the number is divisible by 3 ,if divisible by 5 then display buzz and if divisible by both 3 and 5 then display fizz buzz
+#here we learned about the range where we started from 1 to the last. we incremented by 1 since last value should be displayed and we gapped the valued by 2
+
+
+x=int(input())
+# even_sum=0
+for i in range(1,x+1,2):
+    # even_sum+=i
+   print(i)
+   if(i%3==0):
+    print("fizz")
+   elif(i%5==0):
+    print("buzz")
+   elif(i%3==0 and i%5==0):
+    print("fizzbuzz")
+#    else:
+#     print("nothing")
+
---
+++ 2.py
@@ -0,0 +1,59 @@
+
+# import random.choice 
+import random
+letter=['a','b','c','d']
+number=['1','2','3','4','5','6']
+symbol=['!','@','#','$','%','^','&','*','(',')']
+
+print("create a random password")
+n_letter=int(input("enter the length of the letter"))
+n_number=int(input("enter the lenght of the number"))
+n_symbol=int(input("enter the length of the sybmol"))
+
+# password=""
+#easy format password generator
+# for i in range(1,n_letter+1):
+#       ran=random.choice(letter)
+#       password+=ran
+#     #   print("password")
+    
+# for i in range(1,n_number+1):
+#       ran=random.choice(number)
+#       password+=ran
+#     #   print("password")
+    
+# for i in range(1,n_symbol+1):
+#       ran=random.choice(symbol)
+#       password+=ran
+#     #   print("password")
+
+# print(password)
+
+#hard level password generator
+
+
+password=[]   # here we change string to the list 
+for i in range(1,n_letter+1):
+      ran=random.choice(letter)
+      password+=ran
+    #   print("password")
+    
+for i in range(1,n_number+1):
+      ran=random.choice(number)
+      password+=ran
+    #   print("password")
+    
+for i in range(1,n_symbol+1):
+      ran=random.choice(symbol)
+      password+=ran
+    #   print("password")
+
+print(password)
+random.shuffle(password)
+print(password)
+
+naya_pass=""
+for i in password:
+      naya_pass+=i
+    
+print(f"here your password is : {naya_pass}")  #f stands as the value inside curly bracket ie: { } the result are embidded and provided in string fornmat

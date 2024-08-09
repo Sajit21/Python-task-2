bf3e995e83cf9089802021a55d65878dab133e9b
Sajit Maharjan <126838978+Sajit21@users.noreply.github.com>
2024-08-09 21:24:18 +0545

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,14 @@
+#dictionary
+mydata={"ram":"he is a male ",
+        "sita":"she is a female"}
+
+print(mydata["ram"]) #display the value of the key pair
+
+# mydata={"john":"he is a non-binary"}
+#use of big bracket in the key pair for addition
+mydata["john"]={"he is a non-binary"} #here we add the key value pair to the main variable and dispaly them
+
+print(mydata)
+
+mydata["sita"]={"she is really pretty"} #override the previous data with the new value
+print(mydata)
---
+++ p.py
@@ -0,0 +1,30 @@
+#return with multiple output
+
+def myfun(name1,name2):
+    nam1=name1.title()
+    nam2=name2.title()
+    return f"{nam1} and  {nam2}"
+
+#confused whether we can pass directly the value to the argument 
+hero=myfun("sajit","hari")
+# hero=myfun(name1:"sajit",name2:"hari")
+
+print(hero)
+
+
+#check no is prime or not
+def is_prime(num):
+    if num > 1:
+        is_prime = True  #include the fun is true
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print("The given number is prime")
+        else:
+            print("The number is not a prime")
+    else:
+        print("The number is not a prime")
+
+is_prime(7)

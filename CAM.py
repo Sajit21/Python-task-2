79882a069a963b6eb5aea3e078628dc9322e3856
Sajit Maharjan <126838978+Sajit21@users.noreply.github.com>
2024-08-21 19:10:15 +0545

    Add files via upload

---
+++ CAM.py
@@ -0,0 +1,34 @@
+#only use of class while displaying
+
+class User:
+
+
+#single use of class
+#     pass
+# user1="sajit"
+# user2="hari"
+# print(user1)
+
+#now use of class and constructor
+
+ def __init__(self,user_id,username):
+    self.id=user_id
+    self.name=username
+    self.followers=0       #default value 
+    self.following=0      #default value
+
+ def follows(self,user):
+   user.followers+=1     #maile follow garda kheri user ko follower 1 le badna paryo
+   self.following+=1    #mero following 1 le badna paryo   
+
+user1=User("101","sajit")
+user2=User("101","sajit")
+user2.follows(user1)   #follow method use gareko
+print(user1.followers)
+print(user1.following)
+
+
+print(user1.id)
+print(user1.name)
+
+
